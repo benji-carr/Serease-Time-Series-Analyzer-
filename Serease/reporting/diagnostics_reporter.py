dc6463cbba9995b4f0c0c0ffd8c89347b949698d
Ben Carr <benjicarrfootball@gmail.com>
2025-12-12 15:31:03 -0800

    Fixed minor bug with ReporterConfig class

--- Serease/reporting/diagnostics_reporter.py
+++ Serease/reporting/diagnostics_reporter.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from email.policy import default
 from typing import Any, Dict, List, Optional
 
